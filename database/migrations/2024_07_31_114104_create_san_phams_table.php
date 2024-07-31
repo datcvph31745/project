70579845721febcf5ff16844fb384ca5ac993d19
nguyenvanvu2004 <vu19072k4@gmail.com>
2024-07-31 18:46:17 +0700

    Create 2024_07_31_114104_create_san_phams_table.php

---
+++ database/migrations/2024_07_31_114104_create_san_phams_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('san_phams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('gia');
+            $table->string('image')->nullable();
+            $table->text('mo_ta');
+            $table->unsignedInteger('luot_xem');
+            $table->unsignedBigInteger('id_danh_muc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('san_phams');
+    }
+};
