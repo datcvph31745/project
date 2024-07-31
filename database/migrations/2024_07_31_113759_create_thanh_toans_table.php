2aa2a5231fb029762d29f937a682ddd2c993339e
nguyenvanvu2004 <vu19072k4@gmail.com>
2024-07-31 18:46:06 +0700

    Create 2024_07_31_113759_create_thanh_toans_table.php

---
+++ database/migrations/2024_07_31_113759_create_thanh_toans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('thanh_toans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_users')->unique();
+            $table->unsignedBigInteger('id_gio_hang')->unique();
+            $table->unsignedInteger('sell_off');
+            $table->double('total',10,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('thanh_toans');
+    }
+};
