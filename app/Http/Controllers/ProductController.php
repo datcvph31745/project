a7cf6f3d94f5ffd8adc0819ce50259a8fac1b0f7
nguyenvanvu2004 <vu19072k4@gmail.com>
2024-08-02 19:34:32 +0700

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -30,19 +30,14 @@ class ProductController extends Controller
         return view('products.create', $this->view);
     }
 
-    /**
-     */
-    // pt upload file
+
     private function uploadFile($file){
         $fileName = time()."_".$file->getClientOriginalName();
         return $file->storeAs('image_products', $fileName, 'public');
     }
     public function store(StoreProductRequest $request)
     {
-
-        // loại bỏ trường ảnh
         $data = $request->except('image');
-        // Kiểm xem anh đã được upload thành công
         if($request->hasFile('image') && $request->file('image')->isValid()){
             $data['image'] = $this->uploadFile($request->file('image'));
         }
@@ -55,25 +50,19 @@ class ProductController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(int $id)
     {
-        //
-        // $objCate = new Category();
-        // $this->view['listCate'] = $objCate->loadAllDataCategory();
-        // $objPro = new Product();
-        // $this->view['listPro'] = $objPro->loadIdDataProduct($id);
-        // return view('product.edit', $this->view);
+        
+        $objCate = new Danhmuc();
+        $this->view['listCate'] = $objCate->loadAllDataCategory();
+        $objPro = new Product();
+        $this->view['listPro'] = $objPro->loadIdDataProduct($id);
+        return view('products.edit', $this->view);
     }
 
     /**
@@ -81,33 +70,30 @@ class ProductController extends Controller
      */
     public function update(Request $request, int $id)
     {
-        //
-// //        dd($request->all());
-//         $objPro = new Product();
-//         $checkId = $objPro->loadIdDataProduct($id);
-//         $imageOld = $checkId->image;
-//         if($checkId){
-//             // loại bỏ trường ảnh
-//             $data = $request->except('image');
-//             // Kiểm xem anh đã được upload thành công
-//             if($request->hasFile('image') && $request->file('image')->isValid()){
-//                 $data['image'] = $this->uploadFile($request->file('image'));
-//                 $flag = true;
-//             }else{
-//                 $data['image'] = $imageOld;
-//             }
-//             $res = $objPro->upadateDataProduct($data, $id);
-//             if($res){
-//                     if(isset($imageOld) && Storage::disk('public')->exists($imageOld)){
-//                        Storage::disk('public')->delete($imageOld);
-//                     }
-//                 return redirect()->back()->with('success', 'Sản phẩm chỉnh sửa thành công!');
-//             }else{
-//                 return redirect()->back()->with('error', 'Sản phẩm chỉnh sửa không thành công!');
-//             }
-//         }else{
-//             return redirect()->back()->with('error', 'ID không chính xác!');
-//         }
+        
+        $objPro = new Product();
+        $checkId = $objPro->loadIdDataProduct($id);
+        $imageOld = $checkId->image;
+        if($checkId){
+            $data = $request->except('image');
+            if($request->hasFile('image') && $request->file('image')->isValid()){
+                $data['image'] = $this->uploadFile($request->file('image'));
+                $flag = true;
+            }else{
+                $data['image'] = $imageOld;
+            }
+            $res = $objPro->upadateDataProduct($data, $id);
+            if($res){
+                    if(isset($imageOld) && Storage::disk('public')->exists($imageOld)){
+                       Storage::disk('public')->delete($imageOld);
+                    }
+                return redirect()->back()->with('success', 'Sản phẩm chỉnh sửa thành công!');
+            }else{
+                return redirect()->back()->with('error', 'Sản phẩm chỉnh sửa không thành công!');
+            }
+        }else{
+            return redirect()->back()->with('error', 'ID không chính xác!');
+        }
     }
 
     /**
@@ -115,25 +101,18 @@ class ProductController extends Controller
      */
     public function destroy(int $id)
     {
-        //
-//        dd($id);
-//         $objPro = new Product();
-//         $idCheck = $objPro->loadIdDataProduct($id);
-// //        $imgOld = $idCheck->image;
-//         if($idCheck){
-//             $res = $objPro->deleteDataProduct($id);
-//             if($res){
-// //                if(isset($imgOld)){
-// //                    if(Storage::disk('public')->exists($imgOld)){
-// //                        Storage::disk('public')->delete($imgOld);
-// //                    }
-// //                }
-//                 return redirect()->back()->with('success', 'Sản phẩm xóa thành công!');
-//             }else{
-//                 return redirect()->back()->with('error', 'Sản phẩm xóa không thành công!');
-//             }
-//         }else{
-//             return redirect()->back()->with('error', 'Sản phẩm không tồn tại!');
-//         }
+
+        $objPro = new Product();
+        $idCheck = $objPro->loadIdDataProduct($id);
+        if($idCheck){
+            $res = $objPro->deleteDataProduct($id);
+            if($res){
+                return redirect()->back()->with('success', 'Sản phẩm xóa thành công!');
+            }else{
+                return redirect()->back()->with('error', 'Sản phẩm xóa không thành công!');
+            }
+        }else{
+            return redirect()->back()->with('error', 'Sản phẩm không tồn tại!');
+        }
     }
 }
