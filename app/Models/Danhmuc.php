a193ff2d8d9d45a5d1eb100c6fb4f565a0d06312
nguyenvanvu2004 <vu19072k4@gmail.com>
2024-08-02 17:38:11 +0700

    Create Danhmuc.php

---
+++ app/Models/Danhmuc.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Danhmuc extends Model
+{
+    use HasFactory;
+    protected $table = 'danh_mucs';
+    protected $fillable = [
+        'id',
+        'name',
+        'created_at',
+        'update_at'
+    ];
+    public function loadAllDataCategory(){
+        $query = Category::query()->get();
+        return $query;
+    }
+}
+
