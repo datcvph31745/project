cda65756777b47a944ab364bc0a1ccfd29235130
nguyenvanvu2004 <vu19072k4@gmail.com>
2024-08-02 17:20:27 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -77,10 +77,8 @@ Route::controller(ProductController::class)
         Route::post('/store', 'store')->name('store');
         Route::get('/{id}/edit', 'edit')
         ->name('edit');
-        
         Route::put('/{id}/update', 'update')
         ->name('update');
-
         Route::delete('/{id}/destroy', 'destroy')
         ->name('destroy');
 
