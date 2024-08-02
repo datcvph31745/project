075e6c91582863385454ce9f10dedb5fe09667dd
nguyenvanvu2004 <vu19072k4@gmail.com>
2024-08-02 17:20:15 +0700

    Create Product.php

---
+++ app/Models/Product.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+
+
+    use HasFactory;
+    protected $table = 'san_phams';
+    protected $fillable = [
+        'id',
+        'name',
+        'gia',
+        'so_luong',
+        'image',
+        'id_danh_muc',
+        'created_at',
+        'updated_at'
+    ];
+    public function listCate(){
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+    public function loadAllDataProductWithPager(){
+        // ORM
+        $query = Product::query()
+            ->with('listCate')
+            ->latest('id')
+            ->paginate(10);
+        return $query;
+
+    }
+    public function insertDataProduct($params){
+        $params['created_at'] = date('Y-m-d H:i:s');
+        $res = Product::query()->create($params);
+        return $res;
+    }
+    public function loadIdDataProduct($id){
+        $query = Product::query()->find($id);
+        return $query;
+    }
+    public function deleteDataProduct($id){
+        $query = Product::query()
+            ->find($id)
+            ->delete();
+        return $query;
+    }
+    public function upadateDataProduct($params, $id){
+        $params['updated_at'] = date('Y-m-d H:i:s');
+        $res = Product::query()->find($id)->update($params);
+        return $res;
+
+
+    }
+}
