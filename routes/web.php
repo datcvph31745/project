281bbdbe937ecb3a0ef50e16e9dae77778baf9c4
nguyenvanvu2004 <vu19072k4@gmail.com>
2024-08-05 16:50:14 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,14 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
-<<<<<<< HEAD
 use App\Http\Controllers\ChiTietSanPhamController;
 use App\Http\Controllers\CartController;
-use App\Http\Controllers\OrDerController;
-
-
-
-
-
-
-=======
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\DanhMucController; // Đảm bảo thêm controller này
->>>>>>> dat
+use App\Http\Controllers\OrderController; // Corrected to OrderController
+use App\Http\Controllers\DanhMucController; // Updated to DanhMucController
 
 /*
 |--------------------------------------------------------------------------
@@ -32,6 +21,7 @@ use App\Http\Controllers\DanhMucController; // Đảm bảo thêm controller nà
 |
 */
 
+// Frontend Routes
 Route::get('/', function () {
     return view('frontend.index');
 })->name('home');
@@ -40,16 +30,9 @@ Route::get('/shop', function () {
     return view('frontend.shop');
 })->name('shop');
 
-<<<<<<< HEAD
-    // Route::get('/sproduct', function () {
-    //     return view('frontend.sproduct');
-    // })->name('sproduct');
-    
-=======
 Route::get('/blog', function () {
     return view('frontend.blog');
 })->name('blog');
->>>>>>> dat
 
 Route::get('/about', function () {
     return view('frontend.about');
@@ -67,29 +50,11 @@ Route::get('/sproduct', function () {
     return view('frontend.sproduct');
 })->name('sproduct');
 
+// Authentication Routes
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
 
-<<<<<<< HEAD
-    //
-// Route::controller(UserController::class)
-//     ->group(function (){
-//         Route::get('/register', 'register')->name('register');
-//         Route::post('/register', 'postRegister')->name('postRegister');
-//     });
-// Route::get('login', [UserController::class, 'login'])
-//     ->name('login');
-// Route::post('login', [UserController::class, 'postLogin'])
-//     ->name('postLogin');
-// Route::post('logout', [UserController::class, 'logout'])
-//     ->name('logout');
-
-
-
-
-
-=======
 Route::get('/register', function () {
     return view('auth.register');
 })->name('register');
@@ -99,7 +64,6 @@ Route::get('/dashboard', function () {
 })->name('dashboard');
 
 // Routes for ProductController
->>>>>>> dat
 Route::controller(ProductController::class)
     ->name('products.')
     ->prefix('products/')
@@ -107,53 +71,39 @@ Route::controller(ProductController::class)
         Route::get('/', 'index')->name('index');
         Route::get('/create', 'create')->name('create');
         Route::post('/store', 'store')->name('store');
-<<<<<<< HEAD
-        Route::get('/{id}/edit', 'edit')
-        ->name('edit');
-        Route::put('/{id}/update', 'update')
-        ->name('update');
-        Route::delete('/{id}/destroy', 'destroy')
-        ->name('destroy');
-
-});  
-
-Route::get('/product/detail/{id}', [ChiTietSanPhamController::class, 'chitietSanPham'])
-->name('product.detail');
-
-Route::get('/list-cart', [CartController::class, 'listCart'])->name('cart.list');
-Route::post('/add-to-cart', [CartController::class, 'addCart'])->name('cart.add');
-Route::post('/update-cart', [CartController::class, 'updateCart'])->name('cart.update');
-
-// don hangf
-
-Route::controller(OrDerController::class)
-    ->name('donhangs.')
-    ->prefix('donhangs/')
-    ->group(function(){
-        Route::get('/', 'index')->name('index');
-        Route::get('/create', 'create')->name('create');
-        Route::post('/store', 'store')->name('store');
-        Route::get('/{id}/edit', 'edit')
-        ->name('edit');
-        Route::put('/{id}/update', 'update')
-        ->name('update');
-
-
-});  
-
-
-
-
-
-=======
         Route::get('/{id}/edit', 'edit')->name('edit');
         Route::put('/{id}/update', 'update')->name('update');
         Route::delete('/{id}/destroy', 'destroy')->name('destroy');
     });
->>>>>>> dat
 
-// Routes for DanhMucController (formerly ProductController)
-Route::controller(DanhMucController::class) // Sửa thành DanhMucController
+// Product Detail Route
+Route::get('/product/detail/{id}', [ChiTietSanPhamController::class, 'chitietSanPham'])
+    ->name('product.detail');
+
+// Routes for CartController
+Route::controller(CartController::class)
+    ->name('cart.')
+    ->prefix('cart/')
+    ->group(function () {
+        Route::get('/list', 'listCart')->name('list');
+        Route::post('/add', 'addCart')->name('add');
+        Route::post('/update', 'updateCart')->name('update');
+    });
+
+// Routes for OrderController
+Route::controller(OrderController::class)
+    ->name('orders.')
+    ->prefix('orders/')
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/create', 'create')->name('create');
+        Route::post('/store', 'store')->name('store');
+        Route::get('/{id}/edit', 'edit')->name('edit');
+        Route::put('/{id}/update', 'update')->name('update');
+    });
+
+// Routes for DanhMucController
+Route::controller(DanhMucController::class)
     ->name('danhmucs.')
     ->prefix('danhmucs/')
     ->group(function () {
@@ -163,4 +113,16 @@ Route::controller(DanhMucController::class) // Sửa thành DanhMucController
         Route::get('/{id}/edit', 'edit')->name('edit');
         Route::put('/{id}/update', 'update')->name('update');
         Route::delete('/{id}/destroy', 'destroy')->name('destroy');
+    });
+
+
+    Route::controller(OrderController::class)
+    ->name('donhangs.')
+    ->prefix('donhangs/')
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/create', 'create')->name('create');
+        Route::post('/store', 'store')->name('store');
+        Route::get('/{id}/edit', 'edit')->name('edit');
+        Route::put('/{id}/update', 'update')->name('update');
     });
