f5aca3cf5e112a05b591fd4c0b7f274362a230f0
nguyenvanvu2004 <vu19072k4@gmail.com>
2024-08-06 12:39:42 +0700

    Update index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,55 +4,54 @@
  
 @section('contents')
     @if(session('success'))
-        {{session('success')}}
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
     @endif
     @if(session('error'))
-        {{session('error')}}
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
     @endif
-    <a href="{{route('products.create')}}"
-       class="btn btn-primary" style="width: 13%">Thêm mới </a>
+    <a href="{{ route('products.create') }}" class="btn btn-primary" style="width: 13%">Thêm mới</a>
     <table class="table">
         <thead>
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Tên</th>
-            <th scope="col">Giá</th>
-            <th scope="col">Số lượng</th>
-            <th scope="col">Ảnh</th>
-            <th scope="col">Loại</th>
-            <th scope="col">Thao tác</th>
-        </tr>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Tên</th>
+                <th scope="col">Giá</th>
+                <th scope="col">Số lượng</th>
+                <th scope="col">Ảnh</th>
+                <th scope="col">Loại</th>
+                <th scope="col">Thao tác</th>
+            </tr>
         </thead>
         <tbody>
-        @foreach($listPro as $item)
-        <tr>
-            <td>{{$item->id}}</td>
-            <td>{{$item->name}}</td>
-            <td>{{$item->gia}}</td>
-            <td>{{$item->so_luong}}</td>
-            <td>
-                @if(!isset($item->image))
-                    Không có ảnh
-                @else
-                    <img src="{{Storage::url($item->image)}}" style="width: 100px">
-                @endif
-            </td>
-
-            <td>{{$item->listCate->name}}</td>
-            <td>
-                <form action="{{route('products.destroy', ['id'=>$item->id])}}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" onclick="return confirm('Bạn có chắc chắn muốn xóa???')" class="btn btn-danger">Xóa</button>
-                </form>
-                <a class="btn btn-warning" href="{{route('products.edit', ['id'=> $item->id])}}">Sửa</a>
-
-            </td>
-        </tr>
-        @endforeach
+            @foreach($listPro as $item)
+                <tr>
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->gia }}</td>
+                    <td>{{ $item->so_luong }}</td>
+                    <td>
+                        @if(!$item->image)
+                            Không có ảnh
+                        @else
+                            <img src="{{ Storage::url($item->image) }}" style="width: 100px">
+                        @endif
+                    </td>
+                    <td>{{ $item->listCate->name }}</td>
+                    <td>
+                        <form action="{{ route('products.destroy', ['id' => $item->id]) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" onclick="return confirm('Bạn có chắc chắn muốn xóa???')" class="btn btn-danger">Xóa</button>
+                        </form>
+                        <a class="btn btn-warning" href="{{ route('products.edit', ['id' => $item->id]) }}">Sửa</a>
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
-    {{$listPro->links()}}
-
-
+    {{ $listPro->links() }}
 @endsection
