c6a4bf48af9c682403a700971d2deecaa95bd6fc
nguyenvanvu2004 <vu19072k4@gmail.com>
2024-08-06 12:39:34 +0700

    Update StoreProductRequest.php

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -22,20 +22,42 @@ class StoreProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            // 'name'=>['required', 'string', 'max:255'],
-            // 'gia'=>['required', 'integer', 'min:1'],
-            // 'so_luong'=>['required', 'integer', 'min:1'],
-            // 'image'=>['required', 'image', 'mimes:jpg,png,jpeg', 'max:2048'],
-            // 'id_danh_muc'=>['required', 'exists:categories,id'],
+            'name' => ['required', 'string', 'max:255'],
+            'gia' => ['required', 'integer', 'min:1'],
+            'so_luong' => ['required', 'integer', 'min:1'],
+            'image' => ['required', 'image', 'mimes:jpg,png,jpeg', 'max:2048'],
+            'id_danh_muc' => ['required', 'exists:danh_mucs,id'],
         ];
     }
-    public function messages():array
+
+    /**
+     * Get the custom messages for validation errors.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
     {
         return [
-            // 'name.required' => 'Trường tên không được bỏ trống',
-            // 'name.string' => 'Trường tên yêu bắt buộc là KDL ký tự',
-            // 'name.max' => 'Trường tên không được vươợt quá 255 ký tự',
-            // Lab 5
+            'name.required' => 'Trường tên không được bỏ trống',
+            'name.string' => 'Trường tên bắt buộc phải là kiểu dữ liệu ký tự',
+            'name.max' => 'Trường tên không được vượt quá 255 ký tự',
+
+            'gia.required' => 'Trường giá không được bỏ trống',
+            'gia.integer' => 'Trường giá bắt buộc phải là kiểu số nguyên',
+            'gia.min' => 'Trường giá phải lớn hơn hoặc bằng 1',
+
+            'so_luong.required' => 'Trường số lượng không được bỏ trống',
+            'so_luong.integer' => 'Trường số lượng bắt buộc phải là kiểu số nguyên',
+            'so_luong.min' => 'Trường số lượng phải lớn hơn hoặc bằng 1',
+
+            'image.required' => 'Trường ảnh không được bỏ trống',
+            'image.image' => 'Tệp tải lên phải là hình ảnh',
+            'image.mimes' => 'Ảnh phải có định dạng: jpg, png, jpeg',
+            'image.max' => 'Ảnh không được vượt quá 2048KB',
+
+            'id_danh_muc.required' => 'Trường danh mục không được bỏ trống',
+            'id_danh_muc.exists' => 'Danh mục không tồn tại trong cơ sở dữ liệu',
         ];
     }
 }
+
